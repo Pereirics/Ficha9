b6c383aa9a585b0f9a05fca7e65b11fdf5789ec6
Pereirics <joao.lc.pereira1@gmail.com>
2022-05-12 10:22:05 +0100

    Completo

--- main.c
+++ main.c
@@ -13,7 +13,8 @@ ABin newABin (int r, ABin e, ABin d) {
     }
     return a;
 }
-
+//1.
+//a)
 int max(int x, int y) {
     if (x>y) return x;
     else return y;
@@ -29,6 +30,7 @@ int altura (ABin a) {
     return cont;
 }
 
+//b)
 int nFolhas (ABin a) {
     int cont = 0;
 
@@ -40,6 +42,7 @@ int nFolhas (ABin a) {
     return cont;
 }
 
+//c)
 ABin maisEsquerda (ABin a) {
     if (a != NULL) {
         while (a->esq != NULL) {
@@ -49,6 +52,7 @@ ABin maisEsquerda (ABin a) {
     return a;
 }
 
+//d)
 void imprimeNível(ABin a, int l) {
     if (a != NULL && l > 0) {
        imprimeNível(a->dir, l-1);
@@ -59,12 +63,13 @@ void imprimeNível(ABin a, int l) {
     }
 }
 
+//e)
 int min (int x, int y) {
     if (x < y) return x;
     else return y;
 }
 
-int procuraE (ABin a, int x) {
+int procuraE (ABin a, int x) { //1 é F, 0 é V
     int r = 1;
     if (a != NULL) {
         if (a->valor == x) {
@@ -77,12 +82,60 @@ int procuraE (ABin a, int x) {
     return r;
 }
 
+//2.
+//f)
+ABin result(ABin a, ABin b) {
+    if (a != NULL) return a;
+    else return b;
+}
 
+struct nodo *procura (ABin a, int x) {
+    ABin r = NULL;
+    if (a != NULL) {
+        if (a->valor == x) {
+            r = a;
+        }
+        if (a->valor > x) {
+            r = procura(a->esq, x);
+        }
+        if (a->valor < x) {
+            r = procura(a->dir, x);
+        }
+    }
+    return r;
+}
 
+int nivel (ABin a, int x) {
+    int r;
+    for (r = 0; a != NULL && a->valor != x; r++) {
+        if (a->valor < x) {
+            a = a->dir;
+        }
+        else a = a->esq;
+    }
+    if (a != NULL) return r;
+    else return -1;
+}
 
+void imprimeAte (ABin a, int x) { //Não aparece ordenado
+    if (a != NULL) {
+        if (a->valor < x) {
+            printf("%d ", a->valor);
+        }
+        imprimeAte(a->dir, x);
+        imprimeAte(a->esq, x);
+    }
+}
 
-
-
+void imprimeAte1 (ABin a, int x){ //Versão Carlos, aparece ordenado. Não percebi porquê
+    if(a!=NULL){
+        imprimeAte(a->esq, x);
+        if(a->valor<x){
+            printf("%d ", a->valor);
+            imprimeAte1(a->dir, x);
+        }
+    }
+}
 
 
 
